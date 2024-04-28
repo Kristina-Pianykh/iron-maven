1f56f7a7f7293f38401e2eed6bb1aaf8f78a0608
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-04-28 15:10:07 +0200

    feat: generate a continuous seeded random event stream

--- src/main/java/iron_maven/MockEventGenerator.java
+++ src/main/java/iron_maven/MockEventGenerator.java
@@ -4,69 +4,13 @@ import iron_maven.sources.AtomicEvent;
 
 import java.io.*;
 import java.net.*;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
+import java.util.stream.LongStream;
 
 public class MockEventGenerator {
-  private static final String[] EVENT_TYPES_NODE1 = {
-    "A", "__________", ">>>>>>>>>", "++++++++", "C"
-  };
 
-  private static final String[] EVENT_TYPES_NODE2 = {
-    "A", "__________", "D", ">>>>>>>>>", "++++++++"
-  };
-
-  public static AtomicEvent generateEvent(String nodeNum) {
-    String[] eventTypes = {};
-    if (nodeNum.equals("1")) {
-      eventTypes = EVENT_TYPES_NODE1;
-    } else if (nodeNum.equals("2")) {
-      eventTypes = EVENT_TYPES_NODE2;
-    } else {
-      System.err.println("Invalid node number: " + nodeNum);
-      System.exit(1);
-    }
-    int randomIndex = (int) (Math.random() * eventTypes.length);
-    return new AtomicEvent(eventTypes[randomIndex], String.valueOf(nodeNum));
-  }
-
-  public static void createRandomEventStream(String[] args) {
-    int poolSize = 40;
-    String nodeNum = Niceties.extractStrArg(args, 0);
-    int port = Niceties.extractPort(args, 1);
-    System.out.printf("Sending events to node %d on port %d\n", nodeNum, port);
-    // wait 3 sec for the cep engine to start
-    try {
-      Thread.sleep(3000);
-    } catch (InterruptedException e) {
-      System.out.println("Thread sleep interrupted");
-      e.printStackTrace();
-      System.exit(1);
-    }
-
-    for (int i = 0; i < poolSize; i++) {
-      try (Socket socket = new Socket("localhost", port)) {
-        System.out.println("Connected to server from Client " + i);
-
-        ObjectOutputStream socketOutputStream = new ObjectOutputStream(socket.getOutputStream());
-        AtomicEvent event = generateEvent(nodeNum);
-        socketOutputStream.writeObject(event); // Send message to server
-
-        final ThreadLocalRandom random = ThreadLocalRandom.current();
-        int delay = random.nextInt(300, 2500);
-        Thread.sleep(delay); // simulate a delay
-
-      } catch (IOException e) {
-        e.printStackTrace();
-      } catch (InterruptedException e) {
-        System.out.println("Thread sleep interrupted");
-        e.printStackTrace();
-      }
-    }
-  }
-
-  public static void createDeterministicEventStream(String[] args) {
+  public static void createEventStream(String[] args) {
     String hostname = "localhost";
 
     String nodeID = Niceties.extractStrArg(args, 0);
@@ -91,65 +35,47 @@ public class MockEventGenerator {
     String hostname;
     int nodeID;
     int port;
+    String eventType;
 
     public PortSender(String hostname, int port, String nodeID) {
       this.hostname = hostname;
       this.port = port;
       this.nodeID = Integer.parseInt(nodeID);
+      if (nodeID.equals("1") || nodeID.equals("2")) this.eventType = "C";
+      else if (nodeID.equals("3")) this.eventType = "A";
+      else this.eventType = "D";
     }
 
     @Override
     public void run() {
-      int[] delays;
-      String[] eventTypes;
+      long seed = 12345L; // Fixed seed ensures reproducibility
+      Random random = new Random(seed);
+      LongStream longStream = random.longs(100, 4000);
 
-      int[] delays1 = {3000, 100, 4000};
-      String[] eventTypes1 = new String[delays1.length];
-      Arrays.fill(eventTypes1, "C");
-
-      int[] delays2 = {300, 100, 2400, 800, 900};
-      String[] eventTypes2 = new String[delays2.length];
-      Arrays.fill(eventTypes2, "C");
-
-      int[] delays3 = {450, 250, 700, 500, 169, 480};
-      String[] eventTypes3 = new String[delays3.length];
-      Arrays.fill(eventTypes3, "A");
-
-      int[] delays4 = {750, 550, 150, 360, 700};
-      String[] eventTypes4 = new String[delays4.length];
-      Arrays.fill(eventTypes4, "D");
-
-      if (this.nodeID == 1) {
-        delays = delays1;
-        eventTypes = eventTypes1;
-      } else if (this.nodeID == 2) {
-        delays = delays2;
-        eventTypes = eventTypes2;
-      } else if (this.nodeID == 3) {
-        delays = delays3;
-        eventTypes = eventTypes3;
-      } else {
-        delays = delays4;
-        eventTypes = eventTypes4;
-      }
-
-      int i = 0;
       try (Socket socket = new Socket(this.hostname, this.port)) {
 
-        while (i < delays.length) {
+        longStream.forEach(
+            delay -> {
+              ObjectOutputStream socketOutputStream = null;
+              try {
+                socketOutputStream = new ObjectOutputStream(socket.getOutputStream());
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
+              AtomicEvent event = new AtomicEvent(this.eventType, String.valueOf(nodeID));
+              System.out.println(event);
+              try {
+                socketOutputStream.writeObject(event); // Send message to server
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
 
-          ObjectOutputStream socketOutputStream = new ObjectOutputStream(socket.getOutputStream());
-          AtomicEvent event = new AtomicEvent(eventTypes[i], String.valueOf(nodeID));
-          System.out.println(event);
-          socketOutputStream.writeObject(event); // Send message to server
-
-          try {
-            Thread.sleep(delays[i]); // simulate a delay
-          } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-          }
-          i++;
-        }
+              try {
+                Thread.sleep(delay); // simulate a delay
+              } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+              }
+            });
 
         System.out.println("Closing socket...");
       } catch (UnknownHostException e) {
@@ -162,7 +88,6 @@ public class MockEventGenerator {
   }
 
   public static void main(String[] args) {
-    //    createRandomEventStream(args);
-    createDeterministicEventStream(args);
+    createEventStream(args);
   }
 }
