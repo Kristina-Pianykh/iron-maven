8efb7c04f5f4ed032a5992255123226ca2040ba0
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-04-29 22:06:33 +0200

    wip

--- src/main/java/iron_maven/StreamingJob.java
+++ src/main/java/iron_maven/StreamingJob.java
@@ -13,12 +13,20 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 import org.apache.flink.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// import com.github.loki4j.logback.Loki4jAppender;
 
 import java.util.List;
 import java.util.Map;
 import java.util.HashSet;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class StreamingJob {
+  private static final Logger logger = LoggerFactory.getLogger(StreamingJob.class);
+
   public static HashSet<Integer> processedMatches = new HashSet<>();
 
   public static void main(String[] args) throws Exception {
@@ -26,7 +34,8 @@ public class StreamingJob {
 
     String patternID = Niceties.extractStrArg(args, 0);
     nodeConfig.setString(NodeConf.PATTERN_ID, patternID);
-    System.out.println("Selected pattern: " + patternID);
+    logger.info("Selected patter: {}", patternID);
+    //    System.out.println("Selected pattern: " + patternID);
 
     String nodeID = Niceties.extractStrArg(args, 1);
     nodeConfig.setString(NodeConf.NODE_ID, nodeID);
@@ -39,10 +48,12 @@ public class StreamingJob {
       //      String targetPorts = Niceties.extractStrArg(args, 3);
       List<Integer> targetPorts = Niceties.extractPorts(args, 3);
       nodeConfig.set(NodeConf.TARGET_PORTS, targetPorts);
-      System.out.println("Target ports are set to " + targetPorts);
+      //      System.out.println("Target ports are set to " + targetPorts);
+      logger.info("Target ports are set to {}", targetPorts);
     }
 
-    System.out.println("Initialized Node Config: " + nodeConfig.toString());
+    logger.info("Initialized Node Config: {}", nodeConfig);
+    //    System.out.println("Initialized Node Config: " + nodeConfig.toString());
 
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -91,12 +102,15 @@ public class StreamingJob {
                 System.out.println("\n======================================");
                 for (String key : map.keySet()) {
                   List<AtomicEvent> values = map.get(key);
-                  System.out.println(key + ": " + values);
+                  logger.info(String.format("%s: %s", key, values.toString()));
+                  //                  System.out.println(key + ": " + values);
                   for (AtomicEvent event : values) {
                     Integer hashedEvent = event.hashCode();
                     if (!processedMatches.contains(hashedEvent)) {
-                      System.out.println("Sending event: " + event.toString());
+                      //                      System.out.println("Sending event: " +
+                      // event.toString());
                       processedMatches.add(hashedEvent);
+                      logger.info("Sending event: {}", event.toString());
                       collector.collect(event);
                     }
                   }
@@ -104,11 +118,13 @@ public class StreamingJob {
                 System.out.println("======================================\n");
               }
             });
-    System.out.println(NodeConf.shouldForward((nodeConfig)));
+    logger.info("The Node should forward: {}", NodeConf.shouldForward((nodeConfig)));
+    //    System.out.println(NodeConf.shouldForward((nodeConfig)));
     if (NodeConf.shouldForward(nodeConfig)) {
       matches.addSink(new SocketSink(nodeConfig)).setParallelism(1);
     }
-    System.out.println(env.getExecutionPlan());
+    logger.info(env.getExecutionPlan());
+    //    System.out.println(env.getExecutionPlan());
     env.getExecutionPlan();
     env.execute("CEP Pattern Matching Job");
   }
